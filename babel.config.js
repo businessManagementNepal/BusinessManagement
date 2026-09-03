9ea67deaa9ad8ec242e819256d48f5fdbb3722ba
paras88dhami <parasparepsd@gmail.com>
2026-09-04 00:12:24 +0545

    dd

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
